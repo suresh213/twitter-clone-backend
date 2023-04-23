73a467960086c21dce098c0db4ab0d9b2f741532
suresh213 <asuresh52012@gmail.com>
2023-04-24 03:31:44 +0530

    Add: Types for database models

---
+++ src/db/models/types.ts
@@ -0,0 +1,40 @@
+import { Sequelize } from 'sequelize';
+
+interface Models {
+  [key: string]: any;
+}
+
+interface ModelsInterface {
+  sequelize: Sequelize;
+  Sequelize: typeof Sequelize;
+  [key: string]: any;
+}
+
+interface SequelizeConfigInterface {
+  [key: string]: any;
+}
+
+interface UserInstance {
+  id?: number;
+  name?: string;
+  username?: string;
+  avatar?: string;
+  email: string;
+  password: string;
+}
+
+interface TweetInstance {
+  id?: number;
+  authorId: number;
+  content?: string;
+}
+
+declare const models: ModelsInterface & Models;
+
+export {
+  models,
+  ModelsInterface,
+  SequelizeConfigInterface,
+  UserInstance,
+  TweetInstance,
+};
