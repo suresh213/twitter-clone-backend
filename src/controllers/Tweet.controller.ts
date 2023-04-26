889187358e0170463e556d3c8a6ea02cdb1f78f8
suresh213 <asuresh52012@gmail.com>
2023-04-26 22:26:53 +0530

    Remove: Unwanted functions in tweet controller

--- src/controllers/Tweet.controller.ts
+++ src/controllers/Tweet.controller.ts
@@ -19,11 +19,7 @@ class TweetController {
    * @return {JSON} - JSON Response.
    */
   static async getTweets(req: Request, res: Response) {
-    const { user }: any = req;
-
-    const Tweets = await TweetService.getAll({
-      authorId: user.id,
-    });
+    const Tweets = await TweetService.getAll();
 
     return responseHandler.setSuccess(
       StatusCodes.OK,
@@ -40,9 +36,7 @@ class TweetController {
    * @return {JSON} - JSON Response.
    */
   static async createTweet(req: Request, res: Response) {
-    /**
-     * @type {{title:String,summary:String,link:String}} - body from request
-     */
+    const { user }: any = req;
     const { content } = req.body;
 
     const validationResult = requestParamsHandler.checkParams(
@@ -59,12 +53,9 @@ class TweetController {
     }
 
     try {
-      /**
-       * @type {Object} - Tweet
-       */
       const newTweet = await TweetService.addOne({
         content,
-        authorId: 4,
+        authorId: user.id,
       });
 
       return responseHandler.setSuccess(
@@ -77,105 +68,6 @@ class TweetController {
       return responseHandler.setError(
         StatusCodes.INTERNAL_SERVER_ERROR,
         'Error in Tweet Creation',
-        res
-      );
-    }
-  }
-
-  /**
-   * Update an Tweet.
-   * @param {express.Request<{},{},{},{TweetId:Number}>} req  req - Request object.
-   * @param {express.Response} res - Response Object.
-   * @return {JSON} - JSON Response.
-   */
-  static async updateTweet(req: Request, res: Response) {
-    const alteredTweet = req.body;
-    const { TweetId } = req.query;
-
-    const validationResult = requestParamsHandler.checkParams(
-      ['TweetId'],
-      req.query
-    );
-
-    if (validationResult.error) {
-      return responseHandler.setError(
-        StatusCodes.BAD_REQUEST,
-        validationResult.errorMessage,
-        res
-      );
-    }
-
-    try {
-      const updatedTweet = await TweetService.updateOne(
-        Number(TweetId),
-        alteredTweet
-      );
-
-      if (updatedTweet) {
-        return responseHandler.setSuccess(
-          StatusCodes.OK,
-          'Tweet updated',
-          updatedTweet,
-          res
-        );
-      }
-
-      return responseHandler.setError(
-        StatusCodes.BAD_REQUEST,
-        `Tweet with the TweetId ${TweetId} cannot be found`,
-        res
-      );
-    } catch (err) {
-      return responseHandler.setError(
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        `Error in updating tweet`,
-        res
-      );
-    }
-  }
-
-  /**
-   * Delete an Tweet.
-   * @param {express.Request<{},{},{},{TweetId:Number}>} req  req - Request object.
-   * @param {express.Response} res - Response Object.
-   * @return {JSON} - JSON Response.
-   */
-  static async deleteTweet(req: Request, res: Response) {
-    const { TweetId } = req.query;
-
-    const validationResult = requestParamsHandler.checkParams(
-      ['TweetId'],
-      req.query
-    );
-
-    if (validationResult.error) {
-      return responseHandler.setError(
-        StatusCodes.BAD_REQUEST,
-        validationResult.errorMessage,
-        res
-      );
-    }
-
-    try {
-      const Tweet = await TweetService.deleteOne(Number(TweetId));
-
-      if (Tweet) {
-        return responseHandler.setSuccess(
-          StatusCodes.OK,
-          'Tweet deleted',
-          Tweet,
-          res
-        );
-      }
-      return responseHandler.setError(
-        StatusCodes.BAD_REQUEST,
-        `Tweet with the TweetId ${TweetId} cannot be found`,
-        res
-      );
-    } catch (err) {
-      return responseHandler.setError(
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        `Error in deleting tweet`,
         res
       );
     }
