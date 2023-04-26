4656713602d78e8caf0b8ffa64dc1ff24a8610a5
suresh213 <asuresh52012@gmail.com>
2023-04-26 22:29:47 +0530

    Add: Avatars

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,8 @@
 export const JWT_EXPIRY_TIME = '240000000h';
+
+export const avatars = [
+  'https://bit.ly/ryan-florence',
+  'https://bit.ly/sage-adebayo',
+  'https://bit.ly/kent-c-dodds',
+  'https://bit.ly/code-beast',
+];
