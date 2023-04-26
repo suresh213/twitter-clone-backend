5ddcd8636c00d300eed444517181bf93c0afe0e6
suresh213 <asuresh52012@gmail.com>
2023-04-26 22:28:52 +0530

    Add: Generate username helper function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,3 +4,12 @@ export const validateEmail = (email: string) => {
   const reg = /^([A-Za-z0-9_-.])+@([A-Za-z0-9_-.])+.([A-Za-z]{2,4})$/;
   return reg?.test(email);
 };
+
+export const generateUsername = (name: string): string => {
+  const username = name?.trim()?.toLowerCase().replace(/\s+/g, '');
+
+  const randomNumber = Math.floor(Math.random() * 1000);
+  const uniqueUsername = username + randomNumber;
+
+  return uniqueUsername;
+};
